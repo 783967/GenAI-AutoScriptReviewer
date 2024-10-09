d2b53db58debc9aa452ed1e80ea06f176f541a44
Priyanka2245968 <priyanka.kundu@cognizant.com>
2024-10-09 13:52:53 +0530

    Fetch Pr comments for all repo

---
+++ src/context_setup/FetchPRComments.py
@@ -0,0 +1,29 @@
+import requests
+def triggerGitAPIPullPRComments():
+    headers = {
+        "Accept" : "application/vnd.github+json",
+        "X-GitHub-Api-Version" :"2022-11-28",
+        "Authorization" : "Bearer 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"
+    }
+    githubBaseURL = "https://api.github.com"
+    fetchAllReposURL = "/users/783967/repos"
+    all_repo_details =  requests.get(githubBaseURL + fetchAllReposURL, headers= headers).json()
+    list_all_repos =[]
+    for item in all_repo_details:
+        list_all_repos.append(item["name"])
+    pr_allrepo_dict = []
+    for repo in list_all_repos:
+        githubPullCommentsEndpoint = "/repos/783967/"+repo+"/pulls/comments"
+        response = requests.get(githubBaseURL + githubPullCommentsEndpoint, headers= headers)
+        list_pr_comments = []
+        pr_data = response.json()
+        for item in pr_data: 
+            list_pr_comments.append(item["body"]) 
+        pr_dict = {"repoName" : repo,
+                   "prComments":list_pr_comments
+                  }
+        pr_allrepo_dict.append(pr_dict) 
+    return pr_allrepo_dict
+
+pr_dict = triggerGitAPIPullPRComments()
+print(pr_dict)
