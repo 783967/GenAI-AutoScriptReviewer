5d761bc42169855818de11165ae9f6279acae42d
Priyanka2245968 <priyanka.kundu@cognizant.com>
2024-10-14 13:09:47 +0530

    Removed token

--- src/context_setup/FetchPRComments.py
+++ src/context_setup/FetchPRComments.py
@@ -5,7 +5,7 @@ def triggerGitAPIPullPRComments():
     headers = {
         "Accept" : "application/vnd.github+json",
         "X-GitHub-Api-Version" :"2022-11-28",
-        "Authorization" : "Bearer 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"
+        "Authorization" : "<Token>"
     }
     githubBaseURL = "https://api.github.com"
     fetchAllReposURL = "/users/783967/repos"
