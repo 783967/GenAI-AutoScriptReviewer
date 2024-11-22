9de69cbbd9508a02c98527c36221ee382a6d1847
Ghosh <subhamay.ghosh@cognizant.com>
2024-11-22 12:04:11 +0530

    modified upload file into s3

--- src/context_setup/upload_chroma_to_s3.py
+++ src/context_setup/upload_chroma_to_s3.py
@@ -5,7 +5,7 @@ import boto3
 S3_BUCKET_NAME = "uploadchromadbcontext"
 
 # Initialize S3 client
-s3_client = boto3.client('s3', region_name='US West (Oregon) us-west-2')  # Add your AWS region
+s3_client = boto3.client('s3', region_name='us-west-2')  # Add your AWS region
 
 # This function uploads a file to S3
 def upload_to_s3(file_path, bucket_name, s3_key):
@@ -27,3 +27,8 @@ def upload_chroma_db_to_s3(base_directory):
 if __name__ == "__main__":
     BASE_DIRECTORY = ""  # Local directory path (/path/to/your/local/chroma_db)
     upload_chroma_db_to_s3(BASE_DIRECTORY)
+# Upload specifix file into s3
+# if __name__ == "__main__":
+#     FILE_PATH = r"C:\Users\2280571\Downloads\Sealights Configuration.xlsx"  # Path to your file
+#     S3_KEY = os.path.basename(FILE_PATH)  # S3 key (file name in S3)
+#     upload_to_s3(FILE_PATH, S3_BUCKET_NAME, S3_KEY)
