ce09183d11810bc62e7271659ef6febfcf8d6094
TANMOY NANDY <tanmoy.nandy2@conizant.com>
2024-12-16 16:09:17 +0530

    updated test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import sys
+
 print("This is a test script which fires based on code commit done in SwagLabs Repo")
+
+pr_num_from_arg = sys.argv[1]
+
+print("pr_num_from_arg=",pr_num_from_arg)
+
+pr_num_from_env_var = os.getenv('PR_NUMBER')
+print("pr_num_from_env_var=",pr_num_from_env_var)
